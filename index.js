bb92cd0de6302ff2f92a90fb1703b14b62de4ccf
Kauan Carvalho <kauan3210@gmail.com>
2023-02-22 09:05:30 -0300

    change class = "movies" to id

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 document.getElementById("search-btn").addEventListener('click', ()=>{
     let searchValue = document.getElementById("input-text-movie")
     
@@ -8,11 +12,11 @@ document.getElementById("search-btn").addEventListener('click', ()=>{
 
 
 
-const moviesInfo = (iDimbd) =>{
+let moviesInfo = (iDimbd) =>{
    
     fetch(`https://www.omdbapi.com/?i=${iDimbd}&apikey=29e3c4c9&/`)
          .then(res => res.json())
-         .then(data => console.log(data))
+         .then(data => render(data))
   
      }
 
@@ -32,4 +36,38 @@ const searchMovie = (searchId)=> {
 
 }
 
+/* document.getElementById("movies").innerHTML += `${data.Title}` */
+function render(data){
+    document.getElementById("movies").innerHTML += `
+            <div class="movie-card">
+            <img src="imgs/image 33.png" alt="" class="movie-poster">
 
+                <div class="movie-text">
+
+                    <div class="movie-title">
+                        <h2>
+                            ${data.Title}
+                            <img src="imgs/star.svg" class="star" alt="">
+                            <p>8.1</p>
+                        </h2>
+                    </div>
+
+                    <div class="movie-info">
+                        <p>116min</p>
+                        <p>Drama, Mystery, Sci-fi</p>
+                        <a href="">
+                            <img src="imgs/PlusIcon.svg" class="plus-icon" alt="">
+                            Watchlist
+                        </a>
+                    </div>
+
+                    <p class="movie-description">
+                        A blade runner must pursue and terminate four
+                        replicants who stole a ship in space, and
+                        have returned to Earth to find their creator.
+                    </p> 
+            </div>
+        </div>
+        <div class="line"></div>
+    `
+}
